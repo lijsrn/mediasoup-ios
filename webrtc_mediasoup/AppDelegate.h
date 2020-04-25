de79a15988683168b78d39d434de88bf3c7b9f42
liy313 <1034386099@qq.com>
2020-04-25 16:30:17 +0800

    Initial Commit

---
+++ webrtc_mediasoup/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  webrtc_mediasoup
+//
+//  Created by JH on 2020/4/25.
+//  Copyright © 2020 JH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ webrtc_mediasoup/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  webrtc_mediasoup
+//
+//  Created by JH on 2020/4/25.
+//  Copyright © 2020 JH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
